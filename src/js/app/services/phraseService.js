05948b9560bec2338e485cff9b00f5b50b0bbdeb
Cendrzak, Wojciech (FP) <wcendrzak@future-processing.com>
2015-10-27 20:03:15 +0100

    porting started

--- src/js/app/services/phraseService.js
+++
@@ -1,9 +0,0 @@
-var PhraseService = (function () {
-    function PhraseService() {
-    }
-    PhraseService.prototype.toogleLanguage = function (languageId) {
-    };
-    return PhraseService;
-})();
-exports.PhraseService = PhraseService;
-//# sourceMappingURL=phraseService.js.map
